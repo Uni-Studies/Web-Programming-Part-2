c77dc883cfcb7817ac027270786d69f15ef7b506
Aneliya05 <89069514+Aneliya05@users.noreply.github.com>
2025-10-26 00:51:03 +0300

    some other minutes watched

--- Projects/ProgressTracker/API/Program.cs
+++ Projects/ProgressTracker/API/Program.cs
@@ -6,18 +6,20 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(); // dependency injection
-// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) //puts this in the brackets instead doing it in the controlllers
-//     .AddJwtBearer(options =>
-//     {
-//         options.TokenValidationParameters = new TokenValidationParameters()
-//         {
-//             ValidIssuer = "fmi",
-//             ValidAudience = "front-end",
-//             RequireSignedTokens = false, //put later
-//             IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes
-//                                                         ("!Password123!Password123!Password123"))
-//         };
-//     });
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) //puts this in the brackets instead doing it in the controlllers; controllers will want it for authentication
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters() // specifies how the tokens will be validated
+        {
+            ValidIssuer = "fmi",
+            ValidAudience = "front-end",
+            //RequireSignedTokens = false, //put later
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes
+                                                        ("!Password123!Password123!Password123")) // that is the key for the third section of the jwt token
+            //.net use utf-8 not ASCII
+        };
+    });
+// returns an instance of authentication builder. addJwtBearer is a method giving the options for solving jwt bearer authentication 
 
 var app = builder.Build();
 app.UseHttpsRedirection(); // rejects http requests and wants https
--- Projects/ProgressTracker/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ Projects/ProgressTracker/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea97cabc8f6e27595abc57a3dfbf0856d3d729c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
